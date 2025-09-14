3c45dfe533268070876e36ce323683dc3ee7f15b
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-09-14 23:49:46 +0530

    fix: adjust layout spacing in LoginView for improved visual consistency

--- src/view/auth/LoginView.tsx
+++ src/view/auth/LoginView.tsx
@@ -6,14 +6,14 @@ import { Suspense } from "react";
 const LoginView = () => {
   return (
     <Suspense>
-      <div className="min-h-screen flex justify-center flex-col items-center overflow-hidden">
-        <div>
+      <div className="min-h-screen flex  flex-col items-center overflow-hidden">
+        <div className="mt-5">
           <h1 className="text-4xl font-bold text-center text-primary mt-10">
             Welcome to Plant Diseases Detection System
           </h1>
         </div>
 
-        <div className="flex flex-col items-center mt-5 w-full max-w-md p-6 rounded-lg ">
+        <div className="flex flex-col items-center mt-8 w-full max-w-md p-6 rounded-lg ">
           <LoginForm />
         </div>
       </div>
