bd017d20596657ae307445991748ce991580e3bd
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-09-15 23:46:15 +0530

    suspense added for register page

--- src/view/auth/RegisterView.tsx
+++ src/view/auth/RegisterView.tsx
@@ -1,18 +1,21 @@
 import RegisterForm from "@/components/register/RegisterForm";
 import React from "react";
+import { Suspense } from "react";
 
 const RegisterView = () => {
   return (
-    <div className="flex  items-center min-h-screen flex-col">
-      <div className="mt-5">
-        <h1 className="text-primary text-4xl font-semibold">
-          Plant Diseases Detection System
-        </h1>
+    <Suspense>
+      <div className="flex  items-center min-h-screen flex-col">
+        <div className="mt-5">
+          <h1 className="text-primary text-4xl font-semibold">
+            Plant Diseases Detection System
+          </h1>
+        </div>
+        <div className="flex flex-col items-center mt-8 w-full max-w-md p-6 rounded-lg ">
+          <RegisterForm />
+        </div>
       </div>
-      <div className="flex flex-col items-center mt-8 w-full max-w-md p-6 rounded-lg ">
-        <RegisterForm />
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
